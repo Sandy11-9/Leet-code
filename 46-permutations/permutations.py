b9bf5e4ca053717345a2171a0abf5b8ee61f0ad5
Sandy11-9 <sandeshkalyanshetty09@gmail.com>
2026-07-04 00:08:50 +0530

    Time: 3 ms (55.07%) | Memory: 12.5 MB (56.57%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        li=[]
+        ans=[]
+        self.find(li,ans,nums)
+        return li
+
+    def find(self,li,ans,nums):
+        if(len(ans)==len(nums)):
+            li.append(ans[:])
+            return
+
+
+
+        for i in range(len(nums)):
+            if nums[i] not in ans:
+                ans.append(nums[i])
+                self.find(li,ans,nums)
+                ans.pop(len(ans)-1)
+        
