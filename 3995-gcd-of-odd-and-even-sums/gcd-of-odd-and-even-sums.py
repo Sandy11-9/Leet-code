e41544919c404d0dbda301e68e8f8a8464bf33fe
Sandy11-9 <sandeshkalyanshetty09@gmail.com>
2026-07-15 19:19:29 +0530

    Time: 49 ms (10.07%) | Memory: 12.5 MB (27.85%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def gcdOfOddEvenSums(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        sum_o=0
+        sum_e=0
+        for i in range(1,n*2+1):
+            if i%2==0:
+                sum_e=sum_e+i
+            else:
+                sum_o=sum_o+i
+        while(sum_e!=0):
+            #sum_o,sum_e= sum_e, sum_o % sum_e
+            temp=sum_e
+            sum_e=sum_o % sum_e
+            sum_o=temp
+        return sum_o
+        
